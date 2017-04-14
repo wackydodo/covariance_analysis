40be94222afbffb148f9c049e71206a4b57d8f71
Shu Ye <wackydodotrade@gmail.com>
2017-04-14 15:44:29 -0400

    Update aws_test.py

--- aws_test.py
+++ aws_test.py
@@ -5,5 +5,5 @@ with open('csv_test.csv', 'wb') as csvfile:
 	writer = csv.writer(csvfile)
 	writer.writerow([1, 2, 3])
 
-	data = [('1', '25', '1234567')]
+	data = [(1,3,4)]
 	writer.writerows(data)
