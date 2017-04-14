d6ef0ccaa51c52a60ae49555dcfa7be2957dd6e3
Shu Ye <wackydodotrade@gmail.com>
2017-04-14 15:41:42 -0400

    Update aws_test.py

--- aws_test.py
+++ aws_test.py
@@ -1,14 +1,11 @@
 import csv
 
+with open('csv_test.csv', 'wb') as csvfile:
 
-csvfile = open('csv_test.csv', 'wb')
-writer = csv.writer(csvfile)
-writer.writerow(['a', 'b', 'c'])
+	writer = csv.writer(csvfile)
+	writer.writerow(['a', 'b', 'c'])
 
-data = [
-    ('1', '25', '1234567')
-]
-writer.writerows(data)
-
-csvfile.close()
-
+	data = [
+	    ('1', '25', '1234567')
+	]
+	writer.writerows(data)
